41cd05e3656f92c0bd5a06c416bd8ba4f2571ea8
hoavosac99 <64917842+hoavosac99@users.noreply.github.com>
2021-02-13 20:59:15 +0700

    first commit

---
+++ precache-manifest.4a53748956c84d110a4321254195b480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461859ffed3baa9fd6c44bf214c0a93b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ec7a2455f9761ee8dd",
+    "url": "/static/css/main.134fd2ca.chunk.css"
+  },
+  {
+    "revision": "86565bf88a1944210460",
+    "url": "/static/js/2.27289ddc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27289ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ec7a2455f9761ee8dd",
+    "url": "/static/js/main.dc91e468.chunk.js"
+  },
+  {
+    "revision": "9bd6f6d3aa704e55f1fe",
+    "url": "/static/js/runtime-main.996534eb.js"
+  }
+]);
